d6a249bc8faea53c0a8e01c54e9dbda78687d473
Zeus <zeus@ADMIN-09s-iMac.local>
2025-07-04 11:58:19 +0800

    session09

---
+++ month01/week03/session09/02_python_tuples_exercises.py
@@ -0,0 +1,37 @@
+# X 1
+countries = ("mongolia", "russia", "china")
+print(countries)
+
+# X 2
+numbers = (5, 7, 9)
+print(numbers[1])
+
+# X 3
+numbers = (1, 2, 3)
+# numbers[0] = 4
+# aldaa zaajin 
+
+# X 4
+colors = ("red", "green", "blue")
+color1, color2, color3 = colors
+print(color1)
+print(color2)
+print(color3)
+
+# X 5
+tuple1 = (1, 2)
+tuple2 = (3, 4)
+listtuple1 = list(tuple1)
+listtuple2 = list(tuple2)
+listtuples = listtuple1 + listtuple2
+newtuples = tuple(listtuples)
+print(newtuples)
+
+# or 
+c = tuple1 + tuple2
+print(c)
+
+a = [1, 2]
+b= [3, 4]
+print(a+b)
+# listruu horvuulsen ch shine utgand horvuulj onooj baigaa yu? jinhene tuple utga list ruu horvohgui zov uu
