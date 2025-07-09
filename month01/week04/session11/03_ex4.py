57c99b7d891c48f2fff791f21b7f3618799d60d9
Zeus <zeus@ADMIN-09s-iMac.local>
2025-07-09 12:54:57 +0800

    final

--- month01/week04/session11/03_ex4.py
+++ month01/week04/session11/03_ex4.py
@@ -4,12 +4,16 @@ import random
 colours = ['green', 'DarkGreen', 'green3', 'green4']
 turtle.width(8)
 turtle.speed(0)
+turtle.right(180)
 for n in range(250):
     width = random.randint(200, 300)
-    angle = random.randint(30, 330)
+    angle = random.randint(0, 180)
     turtle.right(angle)
     turtle.color(random.choice(colours))
     turtle.forward(width)
     turtle.right(180)
+    
     turtle.forward(width)
+    turtle.right(180)
+    turtle.left(angle)
 turtle.done()
