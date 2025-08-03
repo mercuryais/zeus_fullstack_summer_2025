77c9d8ca1ab5e250d03df078ebeeddbae7166dd0
mercuryais <kanaobatsuuri@gmail.com>
2025-08-03 23:31:05 +0800

    whatever

--- month02/week06/session17/02_session_17.sql
+++ month02/week06/session17/02_session_17.sql
@@ -5,3 +5,9 @@ INSERT INTO users (id, first_name , surname, salary) VALUES(?, ?, ?, ?), (1, "Ro
 INSERT INTO users (id, first_name , surname, salary) VALUES(?, ?, ?, ?), (2, "Bob", "Smith", 45000);
 
 INSERT INTO users (id, first_name , surname, salary) VALUES(?, ?, ?, ?), (3, "Anne", "Pun", 87000);
+
+SELECT * FROM users WHERE salary >= 60000
+
+SELECT surname FROM users WHERE salary >= 80000 OR age >= 50
+
+SELECT * FROM users WHERE salary >= 35000 AND age <=20 or salary >= 80000 and age <=30
